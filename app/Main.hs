347ce1a63d1d38f0432a4d11d87d2abf16f7954d
Akriti Batra <akritibatra6@gmail.com>
2025-02-19 09:46:28 -0500

    defined data structures

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,27 @@
-module Main where
+data Suit = 
+    Hearts 
+    | Diamonds 
+    | Spades 
+    | Clubs deriving (Show, Eq, Enum)
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+data Rank =
+    Two 
+    | Three 
+    | Four 
+    | Five 
+    | Six 
+    | Seven 
+    | Eight 
+    | Nine 
+    | Ten 
+    | Jack 
+    | Queen 
+    | King 
+    | Ace deriving (Show, Eq, Enum)
+
+data Card = Card Rank Suit deriving (Show, Eq)
+
+deck :: [Card] 
+deck = [Card rank suit | rank <- [Two .. Ace], suit <- [Hearts .. Clubs]] 
+
+
