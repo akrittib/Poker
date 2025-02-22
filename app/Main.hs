088afba738b62303254b46cc594d3783dcc204ea
Akriti Batra <akritibatra6@gmail.com>
2025-02-21 23:43:59 -0500

    added in the evaluate hand portion and did some more debugging but to no avail

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
-import System.Random
-import System.Random.Shuffle
-
+import Random
+import Random.Shuffle
+import Data.List (sort, group, sortBy)
 
 data Suit = Hearts | Diamonds | Spades | Clubs deriving (Show, Eq, Enum, Ord)
 data Rank = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Show, Eq, Enum, Ord)
@@ -31,7 +31,53 @@ data HandRank =
     | RoyalFlush deriving (Show, Eq, Ord)
 
 evaluateHand :: [Card] -> HandRank
-evaluateHand cards = HighCard -- Placeholder for actual hand evaluation logic
+evaluateHand cards
+    | isRoyalFlush cards    = RoyalFlush
+    | isStraightFlush cards = StraightFlush
+    | isFourOfAKind cards   = FourOfAKind
+    | isFullHouse cards     = FullHouse
+    | isFlush cards         = Flush
+    | isStraight cards      = Straight
+    | isThreeOfAKind cards  = ThreeOfAKind
+    | isTwoPair cards       = TwoPair
+    | isPair cards          = Pair
+    | otherwise             = HighCard
+
+isRoyalFlush :: [Card] -> Bool
+isRoyalFlush cards = isStraightFlush cards && maximum ranks == Ace
+  where ranks = map (\(Card rank _) -> rank) cards
+
+isStraightFlush :: [Card] -> Bool
+isStraightFlush cards = isFlush cards && isStraight cards
+
+isFourOfAKind :: [Card] -> Bool
+isFourOfAKind cards = any ((== 4) . length) (group ranks)
+  where ranks = sort $ map (\(Card rank _) -> rank) cards
+
+isFullHouse :: [Card] -> Bool
+isFullHouse cards = any ((== 3) . length) (group ranks) && any ((== 2) . length) (group ranks)
+  where ranks = sort $ map (\(Card rank _) -> rank) cards
+
+isFlush :: [Card] -> Bool
+isFlush cards = all (== head suits) suits
+  where suits = map (\(Card _ suit) -> suit) cards
+
+isStraight :: [Card] -> Bool
+isStraight cards = and $ zipWith (\a b -> fromEnum b == fromEnum a + 1) sortedRanks (tail sortedRanks)
+  where ranks = map (\(Card rank _) -> rank) cards
+        sortedRanks = sort ranks
+
+isThreeOfAKind :: [Card] -> Bool
+isThreeOfAKind cards = any ((== 3) . length) (group ranks)
+  where ranks = sort $ map (\(Card rank _) -> rank) cards
+
+isTwoPair :: [Card] -> Bool
+isTwoPair cards = length (filter ((== 2) . length) (group ranks)) == 2
+  where ranks = sort $ map (\(Card rank _) -> rank) cards
+
+isPair :: [Card] -> Bool
+isPair cards = any ((== 2) . length) (group ranks)
+  where ranks = sort $ map (\(Card rank _) -> rank) cards
 
 data Action = 
      Fold 
